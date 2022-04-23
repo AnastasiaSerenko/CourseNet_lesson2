6c1d3044ab96f03344c10ceafd29b1b51427a856
Anastasia Serenko <orangesun94@gmail.com>
2022-04-23 22:21:15 +0700

    add application

---
+++ ConsoleAppAkulova/ConsoleAppAkulova/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppAkulova { 
+
+    class Program { 
+        static void Main (string[] args){       
+            Console.WriteLine("Let's multiply numbers!\nEnter the first number:");
+            var strNum1 = Console.ReadLine();    
+            
+            Console.WriteLine("Enter the second number:");
+            var strNum2 = Console.ReadLine();    
+
+            if (int.TryParse(strNum1, out var intNum1) && int.TryParse(strNum2, out var intNum2))
+                Console.WriteLine($"\n{intNum1} * {intNum2} = {intNum1 * intNum2}");
+            else
+                Console.WriteLine("Error: incorrect input data");
+            
+            Console.ReadKey();
+        }
+    }
+}
